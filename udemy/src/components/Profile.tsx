c19c68f0e4c7687c465e9dac8e9ace317556c092
Duval <duval.coetzer98@gmail.com>
2021-08-12 23:47:53 +0200

    Added additional functionality and conditional display to the Profile page and Login page

--- udemy/src/components/Profile.tsx
+++ udemy/src/components/Profile.tsx
@@ -1,12 +1,36 @@
 import React from "react";
+import { User, UserAttribute } from "../models/model";
+import { AuthService } from "../services/AuthService";
+import { Link } from "react-router-dom";
 
-export class Profile extends React.Component{
+
+
+interface ProfileState {
+    userAttributes: UserAttribute[]
+}
+interface ProfileProps {
+    user: User | undefined
+    authService: AuthService
+}
+
+export class Profile extends React.Component<ProfileProps, ProfileState>{
 
 
     render(){
+
+        let profileSpace
+        if(this.props.user){
+            profileSpace = <h3>Hello {this.props.user.userName}</h3>
+        } else {
+            profileSpace = <div>
+                Please <Link to='/login'>login</Link> 
+            </div> 
+        }
+
         return(
             <div>
-                Welcome to the profile page!!
+                Welcome to the Profile page!
+                {profileSpace}
             </div>
         )
     }
--- udemy/src/components/login.tsx
+++ udemy/src/components/login.tsx
@@ -1,6 +1,7 @@
 import  React, { SyntheticEvent }  from "react";
 import { User } from "../models/model";
 import { AuthService } from "../services/AuthService";
+import history from "../utilities/history";
 
 
 
@@ -47,6 +48,7 @@ export class Login extends React.Component<LoginProps, LoginState> {
             if (result){
                 this.setState({loginSuccessfull:true})
                 this.props.setUser(result)
+                history.push('/profile')
             } else {
                 this.setState({loginSuccessfull:false})
             }
